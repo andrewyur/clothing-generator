9f3e6374de9c759c39064d7085b56c2aeda4288b
Nicholas Mirigliani <nicholasmirigliani@gmail.com>
2023-09-28 17:36:07 -0400

    cloth rules p1

---
+++ Clothing.py
@@ -0,0 +1,7 @@
+"""The Clothing class contains information on clothing type,
+color, weight, layering priority."""
+class Clothing:
+    def __init__(self, t, c, w, l):
+        self.color = c
+        self.weight = weight
+        self.layerReq
